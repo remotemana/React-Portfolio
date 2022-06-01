38ffb1340aa9ac40b4db4a04bf0fe5575bc8c4b2
Lucas Roman <lucas.e.roman@gmail.com>
2022-05-31 22:54:46 -0700

    still testing

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import "../styles/Portfolio.css";
+import filmSage from "../Assets/Images/Film-Sage.png"
 
 
 export default class Portfolio extends Component {
@@ -7,7 +8,7 @@ export default class Portfolio extends Component {
         return(
             <div>
                 <div>
-                    <a href="https://remotemana.github.io/movie-mood/" target="_blank" rel="noreferrer"><img className="imgOne" alt="Film-Sage" src="../Assets/Images/Film-Sage.png" /></a>
+                    <a href="https://remotemana.github.io/movie-mood/" target="_blank" rel="noreferrer"><img className="imgOne" alt="Film-Sage" src={filmSage} /></a>
                 </div>
             </div>
         )
