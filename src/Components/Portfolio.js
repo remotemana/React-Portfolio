20db9c8157a34ca73e1b046fec56c6a641479ff2
Lucas Roman <lucas.e.roman@gmail.com>
2022-05-31 22:34:28 -0700

    working on portfolio page, need to render an image

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import "../styles/Portfolio.css";
 
 
 export default class Portfolio extends Component {
@@ -6,7 +7,7 @@ export default class Portfolio extends Component {
         return(
             <div>
                 <div>
-                    <a href="https://remotemana.github.io/movie-mood/" target="_blank"><img src="./Assets/Images/Film-Sage.png" /></a>
+                    <a href="https://remotemana.github.io/movie-mood/" target="_blank" rel="noreferrer"><img className="imgOne" alt="Film-Sage" src="../Assets/Images/Film-Sage.png" /></a>
                 </div>
             </div>
         )
